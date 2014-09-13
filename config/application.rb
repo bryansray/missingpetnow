2f739367ea417b1186edea27e11b26089360328e
Bryan Ray <bryan@bryanray.net>
2014-09-13 08:46:21 -0500

    Turning on mongoid logging

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module MissingPetNow
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    Mongoid.logger.level = Logger::DEBUG
+    # Mongoid.logger = Logger.new($stdout)
   end
 end
