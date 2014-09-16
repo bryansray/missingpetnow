5a491acc9c7a6d1c2db1a1bb8096b33f045b377c
Bryan Ray <bryan@bryanray.net>
2014-09-15 22:38:50 -0500

    No need to render these to the view now.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
 	def index
 		@pets = Pet.where(active: true)
-		@lost_pets = Pet.where(active: true, status: :lost)
-		@found_pets = Pet.where(active: true, status: :found)
 	end
 end
